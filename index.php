f040af54e2e0358b889ce387c5d33e281ee5f1b1
rafael-tecinternet <rafah1194@gmail.com>
2022-05-16 08:40:45 -0300

    Index geral e lista de fabricantes

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Vendas</title>
+</head>
+<body>
+    <div class="container">
+        <h1>CRUD - Sistema Vendas</h1>
+        <hr>
+        <p>Operação de <b>inserção</b>,<b>leitura</b>,<b>atualização</b> e <b>exclusão</b> de dados.</p>
+        <ul>
+            <li><a href="fabricantes/listar.php">Fabricantes</a></li>
+            <li><a href="">Produtos</a></li>
+        </ul>
+    </div>
+</body>
+</html>
