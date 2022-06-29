118b3d5d5089d463f0670d988a2229e9a0f65efc
rafael-tecinternet <rafah1194@gmail.com>
2022-06-29 11:57:59 -0300

    Update Utilitarios.php

--- diversos/Utilitarios.php
+++ diversos/Utilitarios.php
@@ -1,6 +1,19 @@
 <?php
 namespace Diversos;
 class Utilitarios {
-    private static float $Moeda;
+    public static float $Moeda;
+    function formataMoeda(float $moeda):string{
+        return "R$ ".number_format($moeda, 2 , ",", ".");
+    }
+    
+
+    public static function getMoeda(): float
+    {
+        return self::$Moeda;
+    }
+    public static function setMoeda(float $Moeda)
+    {
+        self::$Moeda = $Moeda;
+    }
 }
 ?>
