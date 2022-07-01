8d9c5867cc1adab4cfb53e2a80dbdbe09d522a12
rafael-tecinternet <rafah1194@gmail.com>
2022-07-01 09:14:08 -0300

    desafio

--- diversos/Utilitarios.php
+++ diversos/Utilitarios.php
@@ -1,19 +1,14 @@
 <?php
 namespace Diversos;
-class Utilitarios {
-    public static float $Moeda;
-    function formataMoeda(float $moeda):string{
+abstract class Utilitarios {
+    public static float $moeda;
+    public static array $dados;
+    public static function formataMoeda(float $moeda):string{
         return "R$ ".number_format($moeda, 2 , ",", ".");
     }
-    
+    public static function dump($dados){
+        
+    }
 
-    public static function getMoeda(): float
-    {
-        return self::$Moeda;
-    }
-    public static function setMoeda(float $Moeda)
-    {
-        self::$Moeda = $Moeda;
-    }
 }
 ?>
